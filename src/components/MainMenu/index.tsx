b1017e27b153483889993ecb648b66511f1463a9
Estar <estarzhang@foxmail.com>
2023-04-09 10:09:59 +0800

    add antd

---
+++ src/components/MainMenu/index.tsx
@@ -0,0 +1,44 @@
+
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import { Menu, MenuProps } from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+const items: MenuItem[] = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
+
+const MainMenu: React.FC = () => {
+    return (<div>
+
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+
+    </div>)
+}
+export default MainMenu
